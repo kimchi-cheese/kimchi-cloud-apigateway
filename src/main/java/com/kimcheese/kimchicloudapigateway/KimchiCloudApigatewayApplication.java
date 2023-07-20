07c92ca92e568b9d100cfbb6d7ab62539a45b58e
창민 <ckd29672@naver.com>
2023-07-20 10:29:37 +0900

    initialization: git repository 연결

---
+++ src/main/java/com/kimcheese/kimchicloudapigateway/KimchiCloudApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kimcheese.kimchicloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimchiCloudApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimchiCloudApigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimcheese/kimchicloudapigateway/KimchiCloudApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimcheese.kimchicloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimchiCloudApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
